3ef618206d1429147355e39d5e488668c6ac48f4
Bocchio01 <tommaso.bocchietti@gmail.com>
2023-03-30 21:57:29 +0200

    Improved color handling + Better parameterisation

--- Main.c
+++ Main.c
@@ -3,7 +3,7 @@
  *
  * Author: Tommaso Bocchietti
  * Email: tommaso.bocchietti@gmail.com
- * Date: 2023-03-29
+ * Date: 2023-03-30
  *
  * This program let two players play the game of 'Four in a Row' on the same computer.
  * The game is played on a 6x7 board, and the first player to get four of his/her symbols in a row (horizontally, vertically or diagonally) wins.
@@ -21,19 +21,24 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define ROWS 6
-#define COLUMNS 7
+#define ROWS 13
+#define COLUMNS 25
+#define WINNING_SYMBOLS 4
 #define PLAYER_NUMBER 2
 
-#define ANSI_COLOR_RED "\x1b[31m"
-#define ANSI_COLOR_YELLOW "\x1b[33m"
-// #define ANSI_COLOR_BLUE "\x1b[34m"
-#define ANSI_COLOR_RESET "\x1b[0m"
+#define EMPTY_CELL_VALUE -1 // The value of an empty cell in the board. Must be a negative number!
+#define EMPTY_CELL_SYMBOL 46
 
-#define RED(X) ANSI_COLOR_RED X ANSI_COLOR_RESET
-#define YELLOW(X) ANSI_COLOR_YELLOW X ANSI_COLOR_RESET
+#define PRINT_COLOURED(c, f, s) printf("\033[%dm" f "\033[0m", c, s)
 
-void draw_board(int board[ROWS][COLUMNS])
+struct players
+{
+	char name[20];
+	int symbol;
+	int color;
+};
+
+void draw_board(int board[ROWS][COLUMNS], struct players players[PLAYER_NUMBER])
 {
 	system("@cls || clear");
 
@@ -41,25 +46,21 @@ void draw_board(int board[ROWS][COLUMNS])
 	{
 		for (int c = 0; c < COLUMNS; c++)
 		{
-			switch (board[r][c])
-			{
-			case 120:
-				printf(" " RED("%c") " ", board[r][c]);
-				break;
-
-			case 111:
-				printf(" " YELLOW("%c") " ", board[r][c]);
-				break;
-
-			default:
-				printf(" %c ", board[r][c]);
-				break;
-			}
+			if (board[r][c] >= 0)
+				PRINT_COLOURED(players[board[r][c]].color, " %c ", players[board[r][c]].symbol);
+			else
+				printf(" %c ", EMPTY_CELL_SYMBOL);
 		}
 		printf("\n");
 	}
-	printf(" -------------------");
-	printf("\n 1  2  3  4  5  6  7\n");
+
+	for (int i = 0; i < COLUMNS; i++)
+		printf("---");
+	printf("\n");
+
+	for (int i = 0; i < COLUMNS; i++)
+		i + 1 > 9 ? printf(" %d", i + 1) : printf(" %d ", i + 1);
+	printf("\n");
 }
 
 bool is_insert_valid(int board[ROWS][COLUMNS], int selected_column)
@@ -70,7 +71,7 @@ bool is_insert_valid(int board[ROWS][COLUMNS], int selected_column)
 		return false;
 	}
 
-	if (board[ROWS - 1][selected_column] != 46)
+	if (board[ROWS - 1][selected_column] != -1)
 	{
 		printf("\nThe selected column is full");
 		return false;
@@ -86,7 +87,7 @@ int insert_into_board(int (*board)[ROWS][COLUMNS], int symbol, int selected_colu
 		return -1;
 
 	int r = 0;
-	while ((*board)[r][selected_column] != 46)
+	while ((*board)[r][selected_column] != EMPTY_CELL_VALUE)
 		r++;
 	(*board)[r][selected_column] = symbol;
 
@@ -102,7 +103,7 @@ bool check_winner(int board[ROWS][COLUMNS], int position_array[2])
 	{
 		int count = 0;
 
-		for (int dir = -1; dir <= 1; dir += 2)
+		for (int dir = EMPTY_CELL_VALUE; dir <= 1; dir += 2)
 		{
 			int r = position_array[0];
 			int c = position_array[1];
@@ -115,7 +116,7 @@ bool check_winner(int board[ROWS][COLUMNS], int position_array[2])
 			}
 		}
 
-		if (--count >= 4) // Because the central symbol is counted twice
+		if (--count >= WINNING_SYMBOLS) // Because the central symbol is counted twice
 			return true;
 	}
 
@@ -125,7 +126,7 @@ bool check_winner(int board[ROWS][COLUMNS], int position_array[2])
 bool check_tie(int board[ROWS][COLUMNS])
 {
 	for (int c = 0; c < COLUMNS; c++)
-		if (board[ROWS - 1][c] == 46)
+		if (board[ROWS - 1][c] == EMPTY_CELL_VALUE)
 			return false;
 
 	return true;
@@ -138,25 +139,14 @@ int main(int argc, char *argv[])
 	bool debug = argc > 1;
 
 	int board[ROWS][COLUMNS], player_index;
+	struct players players[PLAYER_NUMBER];
 
-	struct players
-	{
-		char name[20];
-		int symbol;
-	} players[PLAYER_NUMBER];
-
-	enum symbol
-	{
-		CROSS = 120,
-		CIRCLE = 111
-	};
+	int symbol_array[] = {'x', 'o', '*', '@', '#', '$'}; // ASCII codes
+	int color_array[] = {31, 33, 32, 34, 35, 36};		 // ANSI color codes
 
 	for (int r = 0; r < ROWS; r++)
 		for (int c = 0; c < COLUMNS; c++)
-			board[r][c] = 46;
-
-	players[0].symbol = CROSS;
-	players[1].symbol = CIRCLE;
+			board[r][c] = EMPTY_CELL_VALUE;
 
 	// Program begin
 	if (!debug)
@@ -170,7 +160,10 @@ int main(int argc, char *argv[])
 		{
 			printf("\nEnter the name of player %d: ", i + 1);
 			scanf("%s", players[i].name);
-			printf("Player %s is going to use: %c\n", players[i].name, players[i].symbol);
+			players[i].symbol = symbol_array[i];
+			players[i].color = color_array[i];
+			printf("Player %s is going to use: ", players[i].name);
+			PRINT_COLOURED(players[i].color, "%c\n", players[i].symbol);
 		}
 
 		print_horizontal_line(50, '-');
@@ -183,7 +176,7 @@ int main(int argc, char *argv[])
 		}
 
 		srand(time(NULL));
-		player_index = rand() % 2;
+		player_index = rand() % PLAYER_NUMBER;
 		printf("\n%s starts! (press any key to continue)", players[player_index].name);
 
 		clear_input_buffer();
@@ -191,8 +184,13 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		strcpy(players[0].name, "Player 1");
-		strcpy(players[1].name, "Player 2");
+		for (int i = 0; i < PLAYER_NUMBER; i++)
+		{
+			sprintf(players[i].name, "Player %d", i + 1);
+			players[i].symbol = symbol_array[i];
+			players[i].color = color_array[i];
+		}
+
 		player_index = 0;
 	}
 
@@ -200,13 +198,15 @@ int main(int argc, char *argv[])
 	while (true)
 	{
 
-		draw_board(board);
+		draw_board(board, players);
 		int selected_column = -1;
 		int inserted_row = -1;
 
 		while (inserted_row == -1)
 		{
-			printf("\n%s (%c) selects any column from 1 to 7: ", players[player_index].name, players[player_index].symbol);
+			printf("\n%s (", players[player_index].name);
+			PRINT_COLOURED(players[player_index].color, "%c", players[player_index].symbol);
+			printf(") selects any column from 1 to 7: ");
 			while (scanf("%d", &selected_column) != 1)
 			{
 				printf("You can enter only integer values: ");
@@ -214,25 +214,25 @@ int main(int argc, char *argv[])
 			}
 			selected_column--; // Because of the array index from 0 to 6
 
-			inserted_row = insert_into_board(&board, players[player_index].symbol, selected_column);
+			inserted_row = insert_into_board(&board, player_index, selected_column);
 		};
 
 		int position_array[2] = {inserted_row, selected_column};
 		if (check_winner(board, position_array))
 		{
-			draw_board(board);
+			draw_board(board, players);
 			printf("\nCongratulations %s, you won! :)", players[player_index].name);
 			break;
 		}
 
 		if (check_tie(board))
 		{
-			draw_board(board);
+			draw_board(board, players);
 			printf("\nOhoh, looks like the game ended in draw...");
 			break;
 		}
 
-		player_index = (player_index + 1) % 2;
+		player_index = (player_index + 1) % PLAYER_NUMBER;
 	};
 
 	printf("\n\n");
